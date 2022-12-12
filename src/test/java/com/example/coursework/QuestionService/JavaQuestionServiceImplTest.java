ce1eb037998da33bf525a45145760fc86c162b6c
sereg <seregapanin23703@gmail.com>
2022-12-12 18:36:03 +0300

    добавил несколько тестов getAll

--- src/test/java/com/example/coursework/QuestionService/JavaQuestionServiceImplTest.java
+++ src/test/java/com/example/coursework/QuestionService/JavaQuestionServiceImplTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,7 +18,9 @@ class JavaQuestionServiceImplTest {
 
     List <Question> listOfQuestion = new ArrayList<>();
     List <Question> listOfQuestion1 = new ArrayList<>();
-    Question question1;
+    Question question1; Question question2; Question question3; Question question4;
+    Question question5; Question question6; Question question7; Question question8;
+    Question question9; Question question10;
 
     public static String QUESTION = "question test";
     public static String ANSWER = "answer test";
@@ -49,6 +52,10 @@ class JavaQuestionServiceImplTest {
 
     @Test
     void questionRemove() {
+        question1 = new Question("Кто был первый в космосе",
+                "Юрий Гагари");
+
+
 
     }
 
@@ -59,6 +66,51 @@ class JavaQuestionServiceImplTest {
 
         Question expected = javaQuestionService.questionAdd(question1);
         assertEquals(question1,expected);
+
+    }
+    @Test
+    void getAll2() {
+
+        List <Question> list= new ArrayList<>();
+        question1 = new Question("Кто был первый в космосе",
+                "Юрий Гагари");
+        list.add(question1);
+        question2 = new Question("Секретный вопрос ",
+                "Пес");
+        list.add(question2);
+        question3 = new Question("Самая высокая точка на земле ",
+                "Эверест");
+        list.add(question3);
+        question4 = new Question("Самая глубокая точка на земле",
+                "Марианская впадина");
+        list.add(question4);
+        question5 = new Question(
+                "Что такое модификаторы доступа в Java? Назовите их. Для чего используются?",
+                "Модификаторы доступа позволяют ограничить доступ к данным для защиты ");
+        list.add(question5);
+        question6 = new Question(
+                "Какая функция у оператора return?",
+                "возвращать результат или использовать для прекращения работы метода");
+        list.add(question6);
+        question7 = new Question(
+                "Когда применяется цикл while?",
+                "когда нам нужен цикл");
+        list.add(question7);
+        question8 = new Question(
+                "Какие есть типы данных в Java?",
+                "примитивные и ссылочные");
+        list.add(question8);
+        question9 = new Question(
+                "Дайте определение классу в Java",
+                "класс это чертеж объекта");
+        list.add(question9);
+        question10 = new Question(
+                "Какие методы называются геттерами? Что они делают?",
+                "позволяют получить данные объекта");
+        list.add(question10);
+
+        Collection<Question> expected = javaQuestionService.getAll();
+        assertEquals(list,expected);
 
     }
 
