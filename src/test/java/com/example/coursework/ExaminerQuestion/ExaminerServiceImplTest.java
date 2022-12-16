ea8f61fcb5c87f74b30e5f48c4b2cad960ae534c
sereg <seregapanin23703@gmail.com>
2022-12-16 23:34:36 +0300

    Исправил  замечания

--- src/test/java/com/example/coursework/ExaminerQuestion/ExaminerServiceImplTest.java
+++ src/test/java/com/example/coursework/ExaminerQuestion/ExaminerServiceImplTest.java
@@ -10,12 +10,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
+import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 class ExaminerServiceImplTest {
 
@@ -28,26 +30,33 @@ class ExaminerServiceImplTest {
     @InjectMocks
     ExaminerServiceImpl examinerService;
 
+
+
     @BeforeEach
     void setUp() {
-        javaQuestionService = new JavaQuestionServiceImpl();
         examinerService = new ExaminerServiceImpl(javaQuestionService);
+        Set<Question> tmp = new HashSet<>(List.of(
+                new Question("Test1", "Test1"),
+                new Question("Test2", "Test2"),
+                new Question("Test3", "Test4")
+        ));
+        when(javaQuestionService.getAll()).thenReturn(tmp);
+        when(javaQuestionService.getRandomQuestion()).thenReturn(new Question(
+                "question1", "answer1"));
+    }
+
+    @Test
+    void shouldGetQuestions1() throws QuestionException {
+        assertEquals(examinerService.getQuestions(1),
+                new ArrayList<>(Set.of(new Question("question1", "answer1"))));
 
     }
 
-
     @Test
-    void shouldGetQuestions() throws QuestionException {
-        List<Question> list = examinerService.getQuestions(2);
+    void shouldGetQuestions2() throws QuestionException {
+        Collection<Question> list = examinerService.getQuestions(1);
 
         assertEquals(list.size(), examinerService.questionsForExam.size());
-    }
-
-    @Test
-    void shouldThrowsException() throws QuestionException {
-
-        assertThrows(QuestionException.class, () -> examinerService.getQuestions(8), "в строку amount " +
-                "введено число больше чем имееться вопросов");
     }
 
 }
